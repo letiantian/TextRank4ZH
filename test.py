e210322074bb8951269891664dfd2d1af7523bea
someus <sunlt1699@gmail.com>
2014-12-01 17:44:04 +0800

    modify readme

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ Created on Dec 1, 2014
 import codecs
 from textrank4zh import TextRank4Keyword, TextRank4Sentence
 
-text = codecs.open('./text/02.txt', 'r', 'utf-8').read()
+text = codecs.open('./text/01.txt', 'r', 'utf-8').read()
 tr4w = TextRank4Keyword(stop_words_file='./stopword.data')
 tr4w.train(text=text, speech_tag_filter=True, lower=True, window=2)
 
@@ -21,4 +21,4 @@ tr4s = TextRank4Sentence(stop_words_file='./stopword.data')
 tr4s.train(text=text, speech_tag_filter=True, lower=True, source = 'all_filters')
 
 print
-print '\n'.join(tr4s.get_key_sentences(num=1))
+print '\n'.join(tr4s.get_key_sentences(num=3))
